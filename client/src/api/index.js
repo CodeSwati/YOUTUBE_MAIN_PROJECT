7985429f7834e844338a7ed686960932467f149c
CodeSwati <swatij280301@gmail.com>
2024-06-03 21:32:56 +0530

    baseURL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL : `http://localhost:5500/`})
+const API = axios.create({baseURL : `https://youtube-main-project.onrender.com`})
 API.interceptors.request.use(req => {
     if(localStorage.getItem('Profile')){
         req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('Profile')).token}`
