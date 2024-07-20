95fb69d1a603c81b21188fb5b74bd90da635b61d
CodeSwati <swatij280301@gmail.com>
2024-07-20 16:42:15 +0530

    video player

---
+++ client/src/actions/point.js
@@ -0,0 +1,23 @@
+
+import * as api from "../api";
+
+export const addpoints=(id)=>async(dispatch)=>{
+    try{
+        const {data}=await api.addpoints(id);
+        console.log(data);
+        dispatch({ type: 'ADD_POINTS', payload:data.points });
+        //dispatch(getAllComment())
+       } catch(error){
+           console.log(error);
+       }
+}
+
+
+// export const fetchUser = (id) => async (dispatch) => {
+//   try {
+//     const { data } = await api.fetchUser(id);
+//     dispatch({ type: 'FETCH_USER', payload: data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
