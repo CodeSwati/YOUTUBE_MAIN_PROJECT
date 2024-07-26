5046680a691dbe86264c01c170e3e388380c4b34
CodeSwati <swatij280301@gmail.com>
2024-07-26 15:34:01 +0530

    video call backend

--- server/index.js
+++ server/index.js
@@ -7,12 +7,21 @@ import bodyParser from "body-parser";
 import videoRoutes from './routes/video.js'
 import commentsRoutes from './routes/comments.js'
 import path from 'path';
+import { Server } from "socket.io";
+import http from "http";
 
 dotenv.config();
 
 const app = express()
 
 app.use(cors())
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: true,
+});
+
+
 app.use(express.json({limit: "30mb" , extended:true }))
 app.use(express.urlencoded({limit: "30mb" , extended:true }));
 app.use('/uploads', express.static(path.join('uploads')))
@@ -27,6 +36,46 @@ app.use('/video', videoRoutes)
 app.use('/comment', commentsRoutes);
 
 const port = process.env.PORT || 5500;
+
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+
+  socket.on("screen-share:start", ({ to }) => {
+    io.to(to).emit("screen-share:start", { from: socket.id });
+  });
+
+  socket.on("screen-share:stop", ({ to }) => {
+    io.to(to).emit("screen-share:stop", { from: socket.id });
+  });
+});
 
 app.listen( port , ()=>{
     console.log(`server running on the port ${port}`)
