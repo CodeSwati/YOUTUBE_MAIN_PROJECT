7f4c8157f2378c6662ed2aaf64e5ae072f56b523
CodeSwati <swatij280301@gmail.com>
2024-07-26 16:09:08 +0530

    vc

--- server/index.js
+++ server/index.js
@@ -14,13 +14,12 @@ dotenv.config();
 
 const app = express()
 
-app.use(cors())
+app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: true,
 });
-
 
 app.use(express.json({limit: "30mb" , extended:true }))
 app.use(express.urlencoded({limit: "30mb" , extended:true }));
